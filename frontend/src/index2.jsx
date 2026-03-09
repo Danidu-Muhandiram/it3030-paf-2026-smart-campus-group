a2c307abf9d25b5d8b710cf84a4e953b0676de78
Danidu Muhandiram <muhandiramdanidu@gmail.com>
2026-03-09 21:13:05 +0530

    test branch files

---
+++ frontend/src/index2.jsx
@@ -0,0 +1,5 @@
+<div className="min-h-screen bg-gray-50 flex items-center justify-center">
+                <div className="text-center">
+                    <h1 className="text-4xl font-bold text-blue-600 mb-4">Smart Campus</h1>
+                </div>
+            </div>
