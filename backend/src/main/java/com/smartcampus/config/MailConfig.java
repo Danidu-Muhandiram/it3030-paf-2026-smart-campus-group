5adf31a4f1c39596c8444f9c816eef8f0de4818e
Danidu Muhandiram <muhandiramdanidu@gmail.com>
2026-04-26 22:52:43 +0530

    fix errors

---
+++ backend/src/main/java/com/smartcampus/config/MailConfig.java
@@ -0,0 +1,14 @@
+package com.smartcampus.config;
+
+public
+package com.smartcampus.config;
+
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@EnableConfigurationProperties(MailProperties.class)
+public class MailConfig {
+}{
+
+import org.springframework.boot.autoconfigure.mail.MailProperties;}
